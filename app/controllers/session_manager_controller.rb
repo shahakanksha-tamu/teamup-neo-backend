3997d0d1f34f3faa287253398a4deb9d9120ba0d
Akanksha Shah <akankshashah@Akankshas-MacBook-Pro.local>
2024-10-01 01:05:49 -0500

    JIRA-26: Add google oAuth callback handler

--- app/controllers/session_manager_controller.rb
+++ app/controllers/session_manager_controller.rb
@@ -1,10 +1,19 @@
 class SessionManagerController < ApplicationController
-  def login
-  end
-
   def logout
   end
 
-  def google_ouath_callback_handler
+  def login(user)
+    session[:user_id] = user.id
+    redirect_to dashboard_path, notice: "You are logged in."
+  end
+
+  def google_oauth_callback_handler
+    auth = request.env["omniauth.auth"]
+    @user = User.find_by(email: auth["info"]["email"], provider: auth["provider"])
+    if @user.present?
+      login @user
+    else
+      redirect_to root_path, alert: "Login failed."
+    end
   end
 end
