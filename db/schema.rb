65396e6eb472f688564a24f616a61d5daa3ac8bb
Yiyang Yan <yyy2000@tamu.edu>
2024-10-03 16:46:53 -0400

    Add Student MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_01_042357) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_033321) do
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
