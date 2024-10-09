340e29679b43de0aa33a0b23bb524c39305688c6
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-09 14:55:41 -0500

    Add stakeholders as admin to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@ users = [
   { first_name: 'Meghana', last_name: 'Pradhan', email: 'meghna.pradhan@tamu.edu', role: 0 },
   { first_name: 'Ramneek', last_name: 'Kaur', email: 'ramneek983@tamu.edu', role: 0 },
   { first_name: 'Hao', last_name: 'Jin', email: 'q389974204@tamu.edu', role: 0 },
-  { first_name: 'Yiyang', last_name: 'Yan', email: 'yyy2000@tamu.edu', role: 0 }
+  { first_name: 'Yiyang', last_name: 'Yan', email: 'yyy2000@tamu.edu', role: 0 },
+  { first_name: 'Florencia', last_name: 'Mangini', email: 'florencia.mangini@teamup.org', role: 'admin' },
+  { first_name: 'David', last_name: 'Kebo', email: 'davidkebo@tamu.edu', role: 'admin' },
+  { first_name: 'Shashankit', last_name: 'Thakur', email: 'shashankit.thakur@gmail.com', role: 'admin' }
 ]
 
 users.each do |user|
