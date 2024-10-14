fe608e8420e4ac1c6ddd9464a755f2d099724a20
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-13 23:51:29 -0500

    JIRA-81: add project name to the team_info view

--- app/controllers/team_info_controller.rb
+++ app/controllers/team_info_controller.rb
@@ -2,14 +2,17 @@
 
 # TeamInfoController displays team member information and key contacts
 class TeamInfoController < ApplicationController
-  def index
+  def index # rubocop:disable Metrics/MethodLength
     current_user_id = @user.id
     student_assignment = StudentAssignment.find_by(user_id: current_user_id)
     @team_members = []
+    @project_name = ''
+
     unless student_assignment.nil?
       project_id = student_assignment.project_id
       other_users_id = StudentAssignment.where(project_id:).where.not(user_id: current_user_id).pluck(:user_id)
       @team_members = User.where(id: other_users_id)
+      @project_name = Project.find_by(id: project_id).name
     end
 
     @mentors = fetch_all_mentors
