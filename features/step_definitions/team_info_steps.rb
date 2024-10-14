30b53c6a0bd94fd2347d5c0e8fd4a080b105d32f
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-14 01:26:47 -0500

    JIRA-82: implemented cucumber scenarios

--- features/step_definitions/team_info_steps.rb
+++ features/step_definitions/team_info_steps.rb
@@ -1,25 +1,80 @@
 # frozen_string_literal: true
 
-Given(/the following projects exist/) do |project_table|
+Given(/the following projects exist/) do |projects|
+  projects.hashes.each do |row|
+    Project.create!(
+      name: row['name'],
+      description: 'Default project description',
+      objectives: 'Default project objectives',
+      status: 'active'
+    )
+  end
 end
 
-Given(/the following student assignments exist/) do |atudent_assignment_table|
+Given(/the following student assignments exist/) do |student_assignment_table|
+  student_assignment_table.hashes.each do |row|
+    user_email = row['user_email']
+    project_name = row['project_name']
+
+    user_id = User.find_by(email: user_email).id
+    project_id = Project.find_by(name: project_name).id
+
+    StudentAssignment.create!(
+      user_id:,
+      project_id:
+    )
+  end
 end
 
 Given('I am logged in as {string}') do |email|
-end
-
-Given('User {string} has project {string} assigned') do |email, project|
+  visit(root_path)
+  mock_omniauth(:google_oauth2, {
+                  provider: 'google_oauth2',
+                  uid: '123456789',
+                  info: {
+                    email:,
+                    name: 'John Doe',
+                    image: 'https://example.com/testuser.jpg'
+                  },
+                  credentials: {
+                    token: 'mock_token',
+                    refresh_token: 'mock_refresh_token',
+                    expires_at: Time.zone.now + 1.week
+                  }
+                })
+  click_button('Login with Google')
 end
 
 When('I visit team information page') do
+  click_link('Team Members')
+end
+
+When('I visit team information page as admin') do
+  visit(dashboard_team_view_path)
 end
 
 Then('I should see mentor information') do
+  mentors = User.where(role: 'admin')
+  mentors.each do |mentor|
+    expect(page).to have_content(mentor.first_name)
+    expect(page).to have_content(mentor.last_name)
+    expect(page).to have_content(mentor.email)
+    expect(page).to have_content(mentor.role.titleize)
+    expect(page).to have_content(mentor.contact)
+  end
 end
 
-Then('I should see team members information for project') do
-end
+Then('I should other team members information for project {string} and not contact information for {string}') do |project_name, email|
+  current_user_id = User.find_by(email:).id
+  project = Project.find_by(name: project_name)
+  puts "Project retrieved: #{project.inspect}"
+  team_members_id = StudentAssignment.where(project_id: project.id).where.not(user_id: current_user_id).pluck(:user_id)
+  team_members = User.where(id: team_members_id)
 
-Given('User {string} has no project assignment') do |email|
+  team_members.each do |member|
+    expect(page).to have_content(member.first_name)
+    expect(page).to have_content(member.last_name)
+    expect(page).to have_content(member.email)
+    expect(page).to have_content(member.contact)
+  end
 end
--- features/team_info.feature
+++ features/team_info.feature
@@ -10,6 +10,7 @@ Background: users in database
   | first_name       | last_name        | email                     |  role         |  contact   |  provider       |
   | John             | Doe              | johndoe@gmail.com         |  student      |  234765456 |  google_oauth2  |
   | Mariam           | Webster          | mariam@gmail.com          |  student      |  123645778 |  google_oauth2  |
+  | Denver           | Kane             | denverkane@gmail.com      |  student      |  123645778 |  google_oauth2  |
   | David            | Jones            | davidjones@gmail.com      |  admin        |  567323423 |  google_oauth2  |
   | Josie            | Mathew           | josie@gmail.com           |  admin        |  214435356 |  google_oauth2  |
 
@@ -25,20 +26,18 @@ Background: users in database
  
 Scenario: Access team member information page as student
   Given I am logged in as "johndoe@gmail.com"
-  Given User "johndoe@gmail.com" has project "Capstone Project" assigned
   When  I visit team information page
   Then  I should see mentor information
-  And   I should see team members information for project
+  And   I should other team members information for project "Capstone Project" and not contact information for "johndoe@gmail.com"
 
 Scenario: Access team member information page as admin
   Given I am logged in as "josie@gmail.com"
-  When  I visit team information page
+  When  I visit team information page as admin
   Then  I should be on the dashboard page
   And   I should see "You are not authorized to access this page"
 
 Scenario: Student has no project assignment
-  Given  I am logged in as "davidjones@gmail.com"
-  And    User "davidjones@gmail.com" has no project assignment
+  Given  I am logged in as "denverkane@gmail.com"
   When   I visit team information page
   Then   I should see "You have a pending project assignment, no team information is available. Please check again later"
 
