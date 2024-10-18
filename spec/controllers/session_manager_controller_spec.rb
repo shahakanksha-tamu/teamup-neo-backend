b4a5ba2609c0cfc090c3604b01ab9c43262f5dd2
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-17 21:32:07 -0500

    JIRA-65: fix rspec for session manager controller

--- spec/controllers/session_manager_controller_spec.rb
+++ spec/controllers/session_manager_controller_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe SessionManagerController, type: :controller do
       role: 'student',
       provider: 'google_oauth2'
     )
+
+    User.create(
+      first_name: 'FN',
+      last_name: 'LN',
+      email: 'testuser2@gmail.com',
+      role: 'admin',
+      provider: 'google_oauth2'
+    )
   end
 
   describe 'GET #logout' do
@@ -39,7 +47,11 @@ RSpec.describe SessionManagerController, type: :controller do
       User.find_by(email: 'testuser@gmail.com')
     end
 
-    context 'when user is found' do
+    let(:admin_user) do
+      User.find_by(email: 'testuser2@gmail.com')
+    end
+
+    context 'when user is student' do
       before do
         request.env['omniauth.auth'] = {
           'info' => { 'email' => user.email },
@@ -47,10 +59,26 @@ RSpec.describe SessionManagerController, type: :controller do
         }
       end
 
-      it 'logs in the user and redirects to the dashboard' do
+      it 'logs in the student and redirects to the dashboard' do
         post :google_oauth_callback_handler
         expect(session[:user_id]).to eq(user.id)
         expect(response).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq('You are logged in.')
+      end
+    end
+
+    context 'when user is admin' do
+      before do
+        request.env['omniauth.auth'] = {
+          'info' => { 'email' => admin_user.email },
+          'provider' => user.provider
+        }
+      end
+
+      it 'logs in the admin and redirects to the project management hub' do
+        post :google_oauth_callback_handler
+        expect(session[:user_id]).to eq(admin_user.id)
+        expect(response).to redirect_to(project_management_hub_path)
         expect(flash[:notice]).to eq('You are logged in.')
       end
     end
