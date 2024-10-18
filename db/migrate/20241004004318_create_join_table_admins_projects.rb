cba22759d34ac20a19849ca71f98fdab58e077f4
HuHuNai <88319603+HuHuNai@users.noreply.github.com>
2024-10-18 15:54:12 -0500

    Delete db/migrate/20241004004318_create_join_table_admins_projects.rb

--- db/migrate/20241004004318_create_join_table_admins_projects.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAdminsProjects < ActiveRecord::Migration[7.2]
-  def change
-    create_join_table :admins, :projects do |t|
-      # t.index [:admin_id, :project_id]
-      # t.index [:project_id, :admin_id]
-    end
-  end
-end
