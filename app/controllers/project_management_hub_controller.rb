678f6133687662f1a3085e5feb0e36db81f11b06
Yiyang Yan <yyy2000@tamu.edu>
2024-10-18 13:10:40 -0400

    Implement admin dashboard controller index method

--- app/controllers/project_management_hub_controller.rb
+++ app/controllers/project_management_hub_controller.rb
@@ -2,5 +2,7 @@
 
 # Handles all functions related to the Project management hub page
 class ProjectManagementHubController < ApplicationController
-  def index; end
+  def index
+    @projects = Project.includes(:users, :timeline, milestones: :tasks)
+  end
 end
