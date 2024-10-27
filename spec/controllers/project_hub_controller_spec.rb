2454a45e83872d10aaa18ecac78824348b81187c
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-26 23:35:15 -0500

    JIRA-98: fix cucumber and rspec test

--- spec/controllers/project_hub_controller_spec.rb
+++ spec/controllers/project_hub_controller_spec.rb
@@ -3,15 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe ProjectHubController, type: :controller do
-  let(:user) { create(:user) }
+  let(:user) do
+    User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
+  end
 
-  context 'when the user is logged in' do
+  context 'when the user is logged in and has no project assignment' do
     before do
       session[:user_id] = user.id
     end
 
     it 'sees project hub dashboard' do
-      user.update(role: :student)
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+
+  context 'when the user is logged in and has a project assignment' do
+    let(:project) { create(:project) }
+
+    before do
+      session[:user_id] = user.id
+      StudentAssignment.create(user_id: user.id, project_id: project.id)
+    end
+
+    it 'sees project hub dashboard' do
       get :index
       expect(response).to render_template(:index)
     end
