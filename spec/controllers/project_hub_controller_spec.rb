3e98f4ce2e6f613d7d70dd47747d2f9ca65218ef
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-28 15:39:40 -0500

    JIRA-98: add rspec for student task management

--- spec/controllers/project_hub_controller_spec.rb
+++ spec/controllers/project_hub_controller_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe ProjectHubController, type: :controller do
-
   describe 'project_hub#index' do
     let(:logged_in_user) do
       User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
@@ -97,6 +96,40 @@ RSpec.describe ProjectHubController, type: :controller do
         expect(assigns(:not_completed_tasks)).to eq([])
         expect(assigns(:completed_tasks)).to eq([])
         expect(assigns(:total_count)).to eq(2)
+      end
+    end
+  end
+
+  describe 'project_hub#update_task_status' do
+    let(:project) { create(:project) }
+
+    let(:logged_in_user) do
+      User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
+    end
+
+    before do
+      session[:user_id] = logged_in_user.id
+      StudentAssignment.create(user_id: logged_in_user.id, project_id: project.id)
+      Milestone.create(project_id: project.id, title: 'milestone 1 title', objective: 'milestone 1 objective', status: 'In-Progress', deadline: '2024-12-12')
+      Task.create(task_name: 'milestone 1 task 1', status: 'Not Started', deadline: '2024-12-10', milestone_id: Milestone.find_by(title: 'milestone 1 title').id, description: 'milestone 1 task 1 description')
+      TaskAssignment.create(user_id: logged_in_user.id, task_id: Task.find_by(task_name: 'milestone 1 task 1').id)
+      allow(controller).to receive(:current_user).and_return(logged_in_user)
+    end
+
+    it 'updates the task status' do
+      patch :update_task_status, params: { project_id: project.id, student_id: logged_in_user.id, id: Task.find_by(task_name: 'milestone 1 task 1').id, status: 'In-Progress' }
+      status = Task.find_by(task_name: 'milestone 1 task 1').status
+      expect(status).to eq('In-Progress')
+    end
+
+    context 'when the update fails' do
+      before do
+        allow_any_instance_of(Task).to receive(:update).and_return(false) # rubocop:disable RSpec/AnyInstance
+      end
+
+      it 'returns the appropriate json' do
+        patch :update_task_status, params: { project_id: project.id, student_id: logged_in_user.id, id: Task.find_by(task_name: 'milestone 1 task 1').id, status: 'Invalid task status' }
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
