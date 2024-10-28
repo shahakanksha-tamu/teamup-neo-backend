cfd0cda062de48de6bdde8776ec1904fa2c749a2
Akanksha Shah <shahakanksha@tamu.edu>
2024-10-28 00:03:49 -0500

    JIRA-98: add rspec for student task management

--- spec/controllers/project_hub_controller_spec.rb
+++ spec/controllers/project_hub_controller_spec.rb
@@ -3,32 +3,101 @@
 require 'rails_helper'
 
 RSpec.describe ProjectHubController, type: :controller do
-  let(:user) do
-    User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
-  end
 
-  context 'when the user is logged in and has no project assignment' do
-    before do
-      session[:user_id] = user.id
+  describe 'project_hub#index' do
+    let(:logged_in_user) do
+      User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
     end
 
-    it 'sees project hub dashboard' do
-      get :index
-      expect(response).to redirect_to(dashboard_path)
+    let(:other_user) do
+      User.create!(first_name: 'Test Name', last_name: 'Test Last Name', contact: '178683473', role: 'student', email: 'testuser@gmail.com')
+    end
+
+    context 'when the user is logged in and has no project assignment' do
+      before do
+        session[:user_id] = logged_in_user.id
+      end
+
+      it 'sees project hub dashboard' do
+        get :index
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    context 'when the user is logged in and has a project assignment' do
+      let(:project) { create(:project) }
+
+      before do
+        session[:user_id] = logged_in_user.id
+        StudentAssignment.create(user_id: logged_in_user.id, project_id: project.id)
+      end
+
+      it 'sees project hub dashboard' do
+        get :index
+        expect(response).to render_template(:index)
+      end
     end
   end
 
-  context 'when the user is logged in and has a project assignment' do
+  describe 'project_hub#view_tasks' do
     let(:project) { create(:project) }
 
-    before do
-      session[:user_id] = user.id
-      StudentAssignment.create(user_id: user.id, project_id: project.id)
+    let(:logged_in_user) do
+      User.create!(first_name: 'First Name', last_name: 'Last Name', contact: '1786839273', role: 'student', email: 'firstname@gmail.com')
     end
 
-    it 'sees project hub dashboard' do
-      get :index
-      expect(response).to render_template(:index)
+    let(:other_user) do
+      User.create!(first_name: 'Test Name', last_name: 'Test Last Name', contact: '178683473', role: 'student', email: 'testuser@gmail.com')
+    end
+
+    before do
+      session[:user_id] = logged_in_user.id
+      StudentAssignment.create(user_id: logged_in_user.id, project_id: project.id)
+      StudentAssignment.create(user_id: other_user.id, project_id: project.id)
+
+      Milestone.create(project_id: project.id, title: 'milestone 1 title', objective: 'milestone 1 objective', status: 'In-Progress', deadline: '2024-12-12')
+      Milestone.create(project_id: project.id, title: 'milestone 2 title', objective: 'milestone 1 objective', status: 'Not Started', deadline: '2024-12-12')
+
+      Task.create(task_name: 'milestone 1 task 1', status: 'Not Started', deadline: '2024-12-10', milestone_id: Milestone.find_by(title: 'milestone 1 title').id, description: 'milestone 1 task 1 description')
+      Task.create(task_name: 'milestone 1 task 2', status: 'In-Progress', deadline: '2024-12-10', milestone_id: Milestone.find_by(title: 'milestone 1 title').id, description: 'milestone 1 task 2 description')
+      Task.create(task_name: 'milestone 1 task 3', status: 'Not Started', deadline: '2024-12-10', milestone_id: Milestone.find_by(title: 'milestone 1 title').id, description: 'milestone 1 task 3 description')
+
+      TaskAssignment.create(user_id: logged_in_user.id, task_id: Task.find_by(task_name: 'milestone 1 task 1').id)
+      TaskAssignment.create(user_id: logged_in_user.id, task_id: Task.find_by(task_name: 'milestone 1 task 2').id)
+      TaskAssignment.create(user_id: other_user.id, task_id: Task.find_by(task_name: 'milestone 1 task 3').id)
+
+      allow(controller).to receive(:current_user).and_return(logged_in_user)
+    end
+
+    context 'when user has tasks assigned' do
+      let(:expected_result) do
+        Task.joins(:task_assignment, :milestone)
+            .where(task_assignments: { user_id: logged_in_user.id })
+            .select('tasks.*, milestones.title, milestones.deadline, milestones.status as milestone_status')
+            .group_by(&:status)
+      end
+
+      it 'shows the sidebar' do
+        get :view_tasks, params: { project_id: project.id, student_id: logged_in_user.id }
+        expect(assigns(:show_sidebar)).to be true
+      end
+
+      it 'loads the logged in user tasks' do
+        get :view_tasks, params: { project_id: project.id, student_id: logged_in_user.id }
+        expected_result = Task.joins(:task_assignment, :milestone)
+                              .where(task_assignments: { user_id: logged_in_user.id })
+                              .select('tasks.*, milestones.title, milestones.deadline, milestones.status as milestone_status')
+                              .group_by(&:status)
+
+        not_started_array = expected_result['Not Started'].sort_by(&:milestone_id)
+        in_progress_array = expected_result['In-Progress'].sort_by(&:milestone_id)
+        expect(assigns(:current_user_tasks)).to eq(expected_result)
+        expect(assigns(:not_started_tasks)).to eq(not_started_array)
+        expect(assigns(:in_progress_tasks)).to eq(in_progress_array)
+        expect(assigns(:not_completed_tasks)).to eq([])
+        expect(assigns(:completed_tasks)).to eq([])
+        expect(assigns(:total_count)).to eq(2)
+      end
     end
   end
 end
