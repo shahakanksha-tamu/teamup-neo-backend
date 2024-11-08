e8e3462bf96400748ad0c5a9aa85322f18a24f5a
Akanksha Shah <shahakanksha@tamu.edu>
2024-11-07 19:12:24 -0600

    JIRA-141: merge dashboard and project hub UI

--- app/controllers/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# DashboardController handles all the methods/functions related to project dashboard
-class DashboardController < ApplicationController
-  def index
-    @projects = Project.joins(:student_assignments)
-                       .where(student_assignments: { user_id: current_user.id })
-  end
-end
