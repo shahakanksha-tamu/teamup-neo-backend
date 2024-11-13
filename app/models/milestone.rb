1558f54bf4eea3b9fc782b7db613da38c4c7705e
Hao Jin <jinhaomirror@gmail.com>
2024-11-13 01:25:53 -0600

    validation only on update

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -4,7 +4,7 @@
 class Milestone < ApplicationRecord
   belongs_to :project
 
-  validate :start_and_end_dates_within_project_dates
+  validate :start_and_end_dates_within_project_dates, on: :update
   validate :status_cannot_be_in_progress_before_start_date, on: :update
 
   has_many :tasks, dependent: :destroy
