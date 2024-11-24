1ead64fe63a3a676310664941fad79d23fca62fc
Yiyang Yan <yyy2000@tamu.edu>
2024-11-24 15:17:31 -0500

    Update rspec test for score controller

--- spec/controllers/score_controller_spec.rb
+++ spec/controllers/score_controller_spec.rb
@@ -2,43 +2,11 @@
 
 require 'rails_helper'
 
-# RSpec.describe ScoreController, type: :controller do
-#   let!(:admin_user) { create(:user, role: :admin) }
-#   let!(:first_student) { create(:user, role: :student, score: 0) }
-#   let!(:second_student) { create(:user, role: :student, score: 0) }
-
-#   before do
-#     session[:user_id] = admin_user.id
-#   end
-
-#   describe 'GET #index' do
-#     it 'renders the edit template' do
-#       get :index
-#       expect(response).to render_template(:index)
-#     end
-#   end
-
-#   describe 'PATCH #update' do
-#     context 'when updating scores' do
-#       it 'updates the scores for the students' do
-#         patch :update, params: { students: { first_student.id.to_s => { score: 95 }, second_student.id.to_s => { score: 88 } } }
-
-#         first_student.reload
-#         second_student.reload
-
-#         expect(first_student.score).to eq(95)
-#         expect(second_student.score).to eq(88)
-#         expect(flash[:notice]).to eq('Score updated successfully!')
-#       end
-#     end
-#   end
-# end
-
 RSpec.describe ScoreController, type: :controller do
   let!(:admin_user) { create(:user, role: :admin) }
   let!(:project) { create(:project) }
-  let!(:first_student) { create(:user, role: :student, score: 0) }
-  let!(:second_student) { create(:user, role: :student, score: 0) }
+  let!(:first_student) { create(:user, role: :student) }
+  let!(:second_student) { create(:user, role: :student) }
 
   before do
     project.users << first_student
@@ -57,7 +25,7 @@ RSpec.describe ScoreController, type: :controller do
   describe 'PATCH #update' do
     context 'when updating scores' do
       it 'updates the scores for the students' do
-        patch :update, params: { project_id: project.id, students: { first_student.id.to_s => { score: 95 }, second_student.id.to_s => { score: 88 } } }
+        patch :update, params: { project_id: project.id, students: { first_student.id.to_s => { score: 95, max_score: 100 }, second_student.id.to_s => { score: 88, max_score: 120 } } }
 
         first_student.reload
         second_student.reload
